059fe9817d7110f359c8955ba6d6cd2264b10cab
Gerasjeka1985 <gekgrom@mail.ru>
2023-12-13 17:12:35 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+@import 'assets/scss/_global.scss';
+
+@include breakpoint(large) {
+
+}
+</style>
+
